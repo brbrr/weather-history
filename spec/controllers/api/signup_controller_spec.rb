16274f53835da294dd991bb8c5f99fd98fd031d9
Iaroslav Kukharuk <i.kukharuk@gmail.com>
2016-12-23 23:27:27 +0800

    include some testing gems, Add some tests

---
+++ spec/controllers/api/signup_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Api::SignupController, type: :controller do
+
+end
---
+++ spec/factories/observation.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :observation do
+    temperature '1'
+    pressure '2'
+    humidity '3'
+  end
+end
